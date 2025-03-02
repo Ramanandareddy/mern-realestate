ea51689120548a489c3212a23d89e5cc1409f6fd
Ramanandareddy Annapureddy <ramanandareddy_a@srmap.edu.in>
2025-03-02 12:12:29 +0530

    MongoDB connected + made express backend

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { FaSearch } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
@@ -14,28 +14,24 @@ function Header() {
         </Link>
       </div>
       <form className="bg-slate-100 p-3 rounded-lg flex items-center max-w-6xl mx-auto">
-        <input
-          type="text"
-          placeholder="search..."
-          className="bg-transparent focus:outline-none w-25 sm:w-64"
+        <input 
+          type="text" 
+          placeholder="search..." 
+          className="bg-transparent focus:outline-none w-25 sm:w-64" 
         />
         <FaSearch className="text-slate-600 ml-2" />
       </form>
       <nav className="max-w-6xl mx-auto mt-4">
         <ul className="flex space-x-4">
-          <Link to="/">
-            <li className="hidden sm:inline text-slate-700 hover:underline">
-              Home
-            </li>
-          </Link>
-          <Link to="/about">
-            <li className="hidden sm:inline text-slate-700 hover:underline">
-              About
-            </li>
-          </Link>
-          <Link to="/sign-in">
-            <li className="text-slate-700 hover:underline">Sign In</li>
-          </Link>
+        <Link to='/'>
+          <li className="hidden sm:inline text-slate-700 hover:underline">Home</li>
+        </Link>
+        <Link to='/about'>
+          <li className="hidden sm:inline text-slate-700 hover:underline">About</li>
+        </Link>
+        <Link to='/sign-in'>
+          <li className="text-slate-700 hover:underline">Sign In</li>
+        </Link>
         </ul>
       </nav>
     </header>
